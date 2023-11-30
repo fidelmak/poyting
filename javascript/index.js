daed831fd61c13ad23921362bc18cba831e4bb62
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2023-12-01 00:57:01 +0100

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,7 @@
-var displayx = "=> Akintunde Oluborode";
+var displayx = " Akintunde Oluborode";
 var i = -2;
 var speed = 50;
+
 function type() {
   if (i < displayx.length) {
     document.getElementById("text").innerHTML += displayx.charAt(i);
@@ -8,6 +9,11 @@ function type() {
     setTimeout(type, 50);
   }
 }
-var display = document.getElementById("text");
-//const newList = document.createTextNode(type());
-display.append(type());
+
+// Call the type() function to display the text
+type();
+
+// No need to append the result of type() since it doesn't return anything
+
+// Optionally, you can use the following line to append a text node to the element
+// var display = document.getElementById("text").appendChild(document.createTextNode(displayx));

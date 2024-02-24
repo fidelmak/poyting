b67b8671bf078ec403dd5955b4ee58a8ed4dfac2
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2024-02-24 11:52:03 +0100

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -13,7 +13,7 @@ function type() {
 // Call the type() function to display the text
 type();
 
-const quotes = [
+const quote = [
   "The only way to do great work is to love what you do. - Steve Jobs",
   "In three words I can sum up everything I've learned about life: it goes on. - Robert Frost",
   "The greatest glory in living lies not in never falling, but in rising every time we fall. - Nelson Mandela",
@@ -37,7 +37,7 @@ const quotes = [
 ];
   
 
-  const quotex=[
+  const quotes=[
 
   "function addTwoNumbers(a, b) { return a + b; }",
 
